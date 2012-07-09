3f42a3d2676c338db6a4bdcc1f38b6245cd5ab4d
Thomas Hallock <github@thomashallock.com>
2012-07-09 01:13:59 -0400

    using express to handle requests.

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 var processing_render_queue = false;
-var app = require('http').createServer(handler)
+var express = require('express')
+  , app = express.createServer()
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(3001);
+app.use(express.static(__dirname + "/public"));
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+app.get("/worker/:for_session",function(req,res) {
+	res.send("hello world " + req.params.for_session);
+});
+// app.get("/",function(req,res) {
+// 	fs.readFile(__dirname + '/master.html',
+// 	function (err, data) {
+// 		if (err) {
+// 			res.writeHead(500);
+// 			return res.end('Error loading index.html');
+// 		}
+// 		res.writeHead(200);
+// 		res.end(data);
+// 	});
+// });
 
 var session_generator = 0;
 var render_queue_start = null;
 var sockets_by_session = {};
 io.sockets.on('connection', function (socket) {
+	var num_live_sockets = 0;
+	for (i in sockets_by_session) {
+		if (!sockets_by_session[i].disconnected) {
+			++num_live_sockets;
+		}
+	}
+	socket.broadcast.emit('node-count', {num_live_sockets:num_live_sockets});
+
+
 	console.log("new connection. session id is " + session_generator);
-	socket.disconnected = false;
+	// socket.disconnected = false;
 	socket.ready_to_render = true;
 	sockets_by_session[session_generator] = socket;
 	console.log("session id = " + session_generator);
@@ -45,13 +57,24 @@ io.sockets.on('connection', function (socket) {
 	});
 	socket.on('disconnect', function (data) {
 		socket.disconnected = true;
+		var num_live_sockets = 0;
+		for (i in sockets_by_session) {
+			if (!sockets_by_session[i].disconnected) {
+				++num_live_sockets;
+			}
+		}
+		socket.broadcast.emit('node-count', {num_live_sockets:num_live_sockets});
 	});
 	socket.on('render-result', function (data) {
 		socket.ready_to_render = true;
 		console.log("got result from session " + data.config.session_id);
 		console.log("here it is: ");
 		console.log(data);
-		sockets_by_session[data.config.session_id].emit('render-result', data);
+		if (sockets_by_session.hasOwnProperty(data.config.session_id)) {
+			sockets_by_session[data.config.session_id].emit('render-result', data);
+		} else {
+			console.log("received render result for non-existent socket: " + session_id);
+		}
 	});
   socket.on('my other event', function (data) {
     console.log(data);
@@ -65,7 +88,7 @@ var dispatch = function(data) {
 	var i;
 	var found_one = false;
 	for (i = 0; i<session_generator; ++i) {
-		if (!sockets_by_session[i].disconnected) {
+		if (sockets_by_session.hasOwnProperty(i) && !sockets_by_session[i].disconnected) {
 			if (sockets_by_session[i].ready_to_render) {
 				if (data.config.session_id == i) {
 					console.log("not sending render request to the render requester, " + i);
@@ -95,7 +118,11 @@ var process_render_queue = function(cur) {
 		}
 	}
 	var did_dispatch;
-	if (sockets_by_session[cur.data.config.session_id].disconnected) {
+	if (!sockets_by_session.hasOwnProperty(cur.data.config.session_id)) {
+		console.log("dispatch attempt to non-existant socket: ");
+		console.log(cur);
+		did_dispatch = true;
+	} else if(sockets_by_session[cur.data.config.session_id].disconnected) {
 		did_dispatch = true;
 	} else if (cur.data) {
 		did_dispatch = dispatch(cur.data);
@@ -120,3 +147,4 @@ var process_render_queue = function(cur) {
 		setTimeout(function() {var _cur = cur; process_render_queue(_cur);},1000);
 	}
 }
+app.listen(80);
